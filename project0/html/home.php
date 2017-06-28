847806459b6edcafd12f43ba40239151d76fc97a
Patrick <599273891@qq.com>
2017-06-28 16:41:41 +0800

    update home.php

--- project0/html/home.php
+++ project0/html/home.php
@@ -32,7 +32,7 @@
     <br>
     <b>Login Demos</b>
     <ul>
-      <li><a href="login1.php">version 1</a></li>
+      <li><a href="Login-single.html">version 1</a></li>
       <li><a href="login2.php">version 2</a></li>
       <li><a href="login3.php">version 3</a></li>
       <li><a href="login4.php">version 4</a></li>
