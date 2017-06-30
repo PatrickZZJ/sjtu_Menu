c71b7407e847b4325c330e556ce903952ac8fe67
Patrick <599273891@qq.com>
2017-06-30 10:14:42 +0800

    update home.php

---
+++ project0/html/menuEditing.php
@@ -0,0 +1,32 @@
+<? 
+    /**
+     * menuEditing.php
+     *
+     * A php file that implement menuEditing usecase.
+     *
+     * Patrick Z
+     * https://github.com/PatrickZZJ/sjtu_Menu
+     */
+
+    // enable sessions
+    session_start();
+?>
+
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Menu Editing</title>
+  </head>
+  <body>
+    <h1>Menu Editing</h1>
+    <br>
+    <b>Login Demos</b>
+    <ul>
+      <li><a href="Login-single.html">version 1</a></li>
+      <li><a href="login2.php">version 2</a></li>
+      <li><a href="login3.php">version 3</a></li>
+      <li><a href="login4.php">version 4</a></li>
+    </ul>
+  </body>
+</html>
