1370d83d1d91c6e4410ea9415c535c054b450a15
Patrick <599273891@qq.com>
2017-06-30 16:07:34 +0800

    logIn implemented

--- project0/php/logIn.php
+++ project0/php/logIn.php
@@ -2,16 +2,11 @@
 登录页切换至点餐页
 -->
 <?php
-
 //处理表单
-if(!empty($_GET['desknumber']))
-{
-	session_start();
-	$_SESSION['desknumber']=$_GET['desknumber'];
-	header('Location:../html/select.html');
-}
-else 
-{
-	header('Location:../html/home.html');
-}
+if(isset($_GET['desknumber']))
+	{
+		session_start();
+		$_SESSION['desknumber']=$_GET['desknumber'];
+		header('Location:select.html');
+	}
 ?>
