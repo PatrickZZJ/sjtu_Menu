2591f1cfeaf333952156dadfc69257d1bdab500d
FEI <fei125@sjtu.edu.cn>
2017-07-02 00:45:18 +0800

    right login

--- project0/php/staffLogIn.php
+++ project0/php/staffLogIn.php
@@ -8,31 +8,23 @@
 	define("CUSER", "isChef");
     define("CPASS", "isCPassword");
 
+	
     // if username and password were submitted, check them
     if (isset($_POST["user"]) && isset($_POST["pass"]))
     {
-        // if username and password are valid, log user in
-        if (($_POST["user"] == MUSER && $_POST["pass"] == MPASS))
+        if (($_POST["user"] == MUSER) && ($_POST["pass"] == MPASS))
         {
-            // remember that user's logged in
             $_SESSION["Mauthenticated"] = true;
 			$_SESSION["staff"]=$_POST["user"];
-
-            $host = $_SERVER["HTTP_HOST"];
-            $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-            header('Location:kitchen.php');
-            exit;
+			header("Location:kitchen.php");
         }
-		if (($_POST["user"] == CUSER && $_POST["pass"] == CPASS))
+	
+		else if (($_POST["user"] == CUSER) && ($_POST["pass"] == CPASS))
         {
-            // remember that user's logged in
             $_SESSION["Cauthenticated"] = true;
 			$_SESSION["staff"]=$_POST["user"];
-			
-            $host = $_SERVER["HTTP_HOST"];
-            $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-            header('Location:kitchen.php');
-            exit;
+			header("Location:kitchen.php");
         }
-    }
+		else {header("Location:staffLogInPage.php");}
+	}
 ?>
