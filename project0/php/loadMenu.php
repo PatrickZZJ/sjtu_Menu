b77d0180bca7abbb1310c2d3aec5d0d4d0cafe69
Montton <1748243264@qq.com>
2017-07-02 20:30:19 +0800

    by Moton

--- project0/php/loadMenu.php
+++ project0/php/loadMenu.php
@@ -37,20 +37,24 @@
 				</li>
 				
 				<li>
-				<form action='menu.php' method='get'>
+				<form action='selectPage.php' method='get' target='_top'>
 					<input type='hidden' name=".$dishIdSet." value='A'>
 					<input type='submit' value='+'>
 				</form>
 				<h3>");
-			$orderId=$_SESSION['n'];
-			$dishSet=$orders->xpath('/orders/order[@id="'.$orderId.'"]/dish[@id="'.$dishIdSet.'"]');
-			if(!isset($dishSet[0]->num))
-				print "0";
-			else
-				print $dishSet[0]->num;
-			
+			$orderId=$orders->ordersNum;
+			$order=$orders->xpath('/orders/order[@id="'.$orderId.'"]');
+			if(isset($order[0]->dish))
+			{
+				$dishSet=$orders->xpath('/orders/order[@id="'.$orderId.'"]/dish[@id="'.$dishIdSet.'"]');
+				if(!isset($dishSet[0]->num))
+					print "0";
+				else
+					print $dishSet[0]->num;
+			}
+			else print "0";
 			print("</h3>
-				<form action='menu.php' method='get'>
+				<form action='selectPage.php' method='get' target='_top'>
 					<input type='hidden' name=".$dishIdSet." value='D'>
 					<input type='submit' value='-' />
 				</form>
