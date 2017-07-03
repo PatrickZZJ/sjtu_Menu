59d78c8542cb1310a41f3141ea1d97182c8967ac
FEI <fei125@sjtu.edu.cn>
2017-07-03 17:57:49 +0800

    类定义

---
+++ project0/classDefine/controllerDefine.php
@@ -0,0 +1,38 @@
+<?php
+
+
+require("entityDefine.php");
+
+class logInController
+{
+	function customerLogInProcess()
+	{
+			$customerOne=new customer($_SESSION['desknumber']);
+			include('../html/selectPage.php');
+	}
+	function staffLogInProcess()
+	{
+		define("MUSER", "isManager");
+		define("MPASS", "isMPassword");
+		define("CUSER", "isChef");
+		define("CPASS", "isCPassword");
+	
+		// if username and password were submitted, check them
+		if (($_POST["user"] == MUSER) && ($_POST["pass"] == MPASS))
+		{
+			$_SESSION["Mauthenticated"] = true;
+			$_SESSION["staff"]=$_POST["user"];
+			header("Location:kitchen.php");
+		}
+	
+		else if (($_POST["user"] == CUSER) && ($_POST["pass"] == CPASS))
+		{
+			$_SESSION["Cauthenticated"] = true;
+			$_SESSION["staff"]=$_POST["user"];
+			header("Location:kitchen.php");
+		}
+		else {header("Location:staffLogInPage.php");}
+	}
+}
+
+?>
