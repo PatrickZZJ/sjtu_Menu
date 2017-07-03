c8c843cddc0948d88fb0c5ac27cf4764112f6584
FEI <fei125@sjtu.edu.cn>
2017-07-03 09:27:08 +0800

    upload new file

--- project0/html/customerLogInPage.php
+++ project0/html/customerLogInPage.php
@@ -1,50 +1,54 @@
+
 <!--
-顾客登陆页面
+
+20170424 Allegro
+	顺便学习html、css的产物。
+	其他：
+	1.做个logo？
+	2.自适应浏览器大小
+	3.增加字体样式
+
+20170630  
+	加了图片，结合bootstrap完成自适应，字体就这样吧……稍微好看点。
+	电脑打开时，中间层背景不透明，手机模拟的时候会透明。
+	
+20170701
+	1.wrap垂直居中
+	2.去掉了jquery
+	
+20170703
+	修改后的页面
 -->
 <?php include('../php/customerLogIn.php')?>
 
 <!DOCTYPE html>
 <html >
 <head>
-  <meta charset="utf-8">
-<link href="jquery-mobile/jquery.mobile.theme-1.3.0.min.css" rel="stylesheet" type="text/css">
-<link href="jquery-mobile/jquery.mobile.structure-1.3.0.min.css" rel="stylesheet" type="text/css">
-<link href="jQueryAssets/jquery.ui.core.min.css" rel="stylesheet" type="text/css">
-<link href="jQueryAssets/jquery.ui.theme.min.css" rel="stylesheet" type="text/css">
-<link href="jQueryAssets/jquery.ui.button.min.css" rel="stylesheet" type="text/css">
-<link href="css.css" rel="stylesheet" type="text/css">
-<script src="jquery-mobile/jquery-1.11.1.min.js"></script>
-<!--<script src="jquery-mobile/jquery.mobile-1.3.0.min.js"></script>-->
-<script src="jQueryAssets/jquery.ui-1.10.4.button.min.js"></script>
+		<meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="format-detection" content="telephone=no">
+		<link rel="stylesheet" href="css\demo.css">                           <!-- 自己写-->
+        <link rel="stylesheet" href="css\frozen.css">
 <title>顾客登录</title>
   
-  <style type="text/css">
-.form-1 {
-  max-width: 1000px;
-  padding: 60px 140px 180px;
-  margin: 0 auto;
-  background-color: #E0E0E0;
-  border: 1px solid rgba(0, 0, 0, 0);
-</style>
+
 </head>
 
 <body>
 
-<div style="text-align: center" data-role="page" id="page">
-		<form class="form-1" action="customerLogInPage.php" > 
-		<img src="images/timg.jpg" width="238" height="215" alt=""/>
+<div class="wrapper" align="center">
+		<form class="form-1" align="center" action="customerLogInPage.php" > 
+		<br>
+		<img src="images/timg.jpg" width="180" height="160"/>
 		<h1>一起用餐吧</h1>
-		<div data-role="fieldcontain">
-			<input  class="form-2" type="number" min="1" max="300" name="desknumber" placeholder="请输入桌号" />      
+		<br/><br/>
+		 <div class="ui-input ui-border-radius">
+      <input type="number" min="1" max="300" name="desknumber" placeholder="请输入桌号" />      
 		</div>
-		<a href="selectPage.php"><button type="submit">开始点餐</button></a>   
+	 <br>
+      <button  class="ui-btn" type="submit">开始点餐</button>   
     </form>
   </div>
 
-<address>
-<br />
-Software Engineering, Group 9 <br />
-</address>
- 
 </body>
 </html>
