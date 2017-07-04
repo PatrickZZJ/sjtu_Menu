44008e3cd3f9732c6080e07a723135305ef53472
Montton <1748243264@qq.com>
2017-07-04 16:04:30 +0800

    get soldOut intformation

--- project0/php/loadMenu.php
+++ project0/php/loadMenu.php
@@ -25,43 +25,46 @@
 		{
 			$dishId=$dish->attributes();
 			$dishIdSet=$dishId['id'];
-			print("			<li class='ui-border-t'>
-			<ul class='ui-tiled'>
-				<div class='ui-list-thumb'>
-				<span style='background-image:url(".$dish->picture.")'></span>
-				</div>
-
-				<li style='padding:15px 0'>
-				<h4>".$dish->Cname."</h4>
-				<h4>".$dish->price."元</h4>
-				</li>
-				
-				<li>
-				<form action='selectPage.php' method='get' target='_top'>
-					<input type='hidden' name=".$dishIdSet." value='A'>
-					<input type='hidden' name='dishctg' value='".$ctgid[0]."' />
-					<input type='submit' value=' ＋ '>
-				</form>
-				<h3>");
-			$orderId=$orders->ordersNum;
-			$order=$orders->xpath('/orders/order[@id="'.$orderId.'"]');
-			if(isset($order[0]->dish))
+			if($dish->soldOut == "否")
 			{
-				$dishSet=$orders->xpath('/orders/order[@id="'.$orderId.'"]/dish[@id="'.$dishIdSet.'"]');
-				if(!isset($dishSet[0]->num))
-					print "0";
-				else
-					print $dishSet[0]->num;
+				print("			<li class='ui-border-t'>
+				<ul class='ui-tiled'>
+					<div class='ui-list-thumb'>
+					<span style='background-image:url(".$dish->picture.")'></span>
+					</div>
+
+					<li style='padding:15px 0'>
+					<h4>".$dish->Cname."</h4>
+					<h4>".$dish->price."元</h4>
+					</li>
+					
+					<li>
+					<form action='selectPage.php' method='get' target='_top'>
+						<input type='hidden' name=".$dishIdSet." value='A'>
+						<input type='hidden' name='dishctg' value='".$ctgid[0]."' />
+						<input type='submit' value=' ＋ '>
+					</form>
+					<h3>");
+				$orderId=$orders->ordersNum;
+				$order=$orders->xpath('/orders/order[@id="'.$orderId.'"]');
+				if(isset($order[0]->dish))
+				{
+					$dishSet=$orders->xpath('/orders/order[@id="'.$orderId.'"]/dish[@id="'.$dishIdSet.'"]');
+					if(!isset($dishSet[0]->num))
+						print "0";
+					else
+						print $dishSet[0]->num;
+				}
+				else print "0";
+				print("</h3>
+					<form action='selectPage.php' method='get' target='_top'>
+						<input type='hidden' name=".$dishIdSet." value='D'/>
+						<input type='hidden' name='dishctg' value='".$ctgid[0]."' />
+						<input type='submit' value=' — ' />
+					</form>
+					</li>
+				</ul>");
 			}
-			else print "0";
-			print("</h3>
-				<form action='selectPage.php' method='get' target='_top'>
-					<input type='hidden' name=".$dishIdSet." value='D'/>
-					<input type='hidden' name='dishctg' value='".$ctgid[0]."' />
-					<input type='submit' value=' — ' />
-				</form>
-				</li>
-			</ul>");
 		}
 	}
 ?>
