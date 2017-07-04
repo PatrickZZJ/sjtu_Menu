39a6d9b96309d41905478241cc73c9701b15402d
allegroxs <allegroxs@sjtu.edu.cn>
2017-07-04 14:35:26 +0800

    修正右上角链接

--- project0/html/kitchen.php
+++ project0/html/kitchen.php
@@ -36,9 +36,9 @@
 		<i class="glyphicon glyphicon-chevron-down"></i>
 			</a>
 		<ul class="dropdown-menu">
-	<li><a href="" target="watch"><span>个人信息</span></a></li>
-	<li><a href="" target="watch"><span>用户设置</span></a></li>
-	<li><a href=""><span>退出系统</span></a></li>
+	<li><a href="staffInfo.html" target="watch"><span>个人信息</span></a></li>
+	<li><a href="settings.html" target="watch"><span>用户设置</span></a></li>
+	<li><a href="staffLogInPage.php"><span>退出系统</span></a></li>
 		</ul></li></ul></div></div></nav>
 		<div id="left2">
 	<nav>
