b5ef5e9a78be57cbd4a8d1ec23c981c0d7a22ba7
tigerny <531907384@qq.com>
2017-07-06 11:37:34 +0800

    add a orderErro Page

---
+++ project0/html/orderErro.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="format-detection" content="telephone=no">
+		<title>订单已被付款</title>
+        <link rel="stylesheet" href="css\demo.css">                           <!-- 自己写-->
+        <link rel="stylesheet" href="css\frozen.css">
+</head>
+
+<body>
+		<header class="ui-header ui-header-positive ui-border-b">
+		<h1> 商家名称：一起用餐吧 </h1>
+        </header>
+	<!--	<footer class="ui-footer  ui-footer-btn ui-btn-group">
+			<ul class="ui-tiled ui-border-t">
+				<li><h1>桌号xxx</h1></li>
+				<li><h1>￥xx.xx</h1></li>
+				<li></li>
+			</ul>
+		</footer>
+	-->	
+		<section class="ui-container ui-center">
+		<div class="wrapper" style="width:40%">
+		<div>		
+		<i class="ui-icon-success"></i>
+		手慢啦，您的伙伴已买单
+		<br />
+		<br />
+		<button class="ui-btn-lg ui-btn-danger" onclick="location.href='customerLogInPage.php'">退出<h2></button>
+		</div>
+		</div>
+		</section>
+		
+
+</body>
+</html>
